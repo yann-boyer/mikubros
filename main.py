078f4cb886c0d394b717c5a758a6af49ee78a988
richou672005 <richou672005@gmail.com>
2021-11-03 15:32:30 +0100

    base window

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ if not pygame.get_init():
 
 pygame.display.set_caption("mikubros")
 pygame.display.set_mode((1280, 720))
+
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("closing mikubros")
